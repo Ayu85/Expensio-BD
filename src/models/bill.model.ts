0fff720c8575a874fb3c0f71d5006137567a12b5
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-23 19:51:28 +0530

    bill model done

---
+++ src/models/bill.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+
+const billSchema = new Schema({
+    category: {
+        type: String, enum: ['water', 'electricity', 'recharge', 'subscriptions', 'wifi', 'credit card']
+    },
+    name: {
+        type: String, required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    status: {
+        type: String, enum: ['due', 'paid'], default: 'due'
+    },
+    duedate: {
+        type: Date,
+        validate: {
+            validator: function (v: any) {
+                return v >= new Date(); // Ensure due date is today or in the future
+            },
+            message: 'Due date must be in the future!'
+        }
+    }
+    ,
+    amount: {
+        type: Number, required: true
+    }
+}, { timestamps: true })
+
+export default model('Bill', billSchema)
